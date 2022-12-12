9de090089af68c0498822fb73eade60574340210
saniazt <saniazt@gmail.com>
2022-12-12 12:32:18 +0200

    12.12.2022 add DTO

--- src/main/java/com/saniazt/restapi/RestApp1/controllers/PeopleController.java
+++ src/main/java/com/saniazt/restapi/RestApp1/controllers/PeopleController.java
@@ -1,12 +1,14 @@
 package com.saniazt.restapi.RestApp1.controllers;
 
 
+import com.saniazt.restapi.RestApp1.dto.PersonDTO;
 import com.saniazt.restapi.RestApp1.models.Person;
 import com.saniazt.restapi.RestApp1.services.PeopleService;
 import com.saniazt.restapi.RestApp1.util.PersonErrorResponse;
 import com.saniazt.restapi.RestApp1.util.PersonNotCreatedException;
 import com.saniazt.restapi.RestApp1.util.PersonNotFoundException;
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,28 +17,33 @@ import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/people")
 public class PeopleController {
     private final PeopleService peopleService;
+    private final ModelMapper modelMapper;
+
 
     @Autowired
-    public PeopleController(PeopleService peopleService) {
-        this.peopleService = peopleService;}
+    public PeopleController(PeopleService peopleService, ModelMapper modelMapper) {
+        this.peopleService = peopleService;
+        this.modelMapper = modelMapper;
+    }
 
     @GetMapping()
-    public List<Person> getPeople(){
-        return peopleService.findAll(); //Jackson automatically convert objects to JSON
+    public List<PersonDTO> getPeople(){
+        return peopleService.findAll().stream().map(this::convertToPersonDTO).collect(Collectors.toList()); //Jackson automatically convert objects to JSON
         }
 
         @GetMapping("/{id}")
-        public Person getPerson(@PathVariable("id") int id){
-        return  peopleService.findOne(id);
+        public PersonDTO getPerson(@PathVariable("id") int id){
+        return  convertToPersonDTO(peopleService.findOne(id));
         }
 
         @PostMapping
-        public ResponseEntity<HttpStatus> create(@RequestBody @Valid Person person,
+        public ResponseEntity<HttpStatus> create(@RequestBody @Valid PersonDTO personDTO,
                                                  BindingResult bindingResult){
             if(bindingResult.hasErrors()) {
                 StringBuilder errorsMsg = new StringBuilder();
@@ -46,12 +53,14 @@ public class PeopleController {
                }
                 throw new PersonNotCreatedException(errorsMsg.toString());
             }
-            peopleService.save(person);
+            peopleService.save(convertToPerson(personDTO));
             return ResponseEntity.ok(HttpStatus.OK); //ок тк не хотим создавать отдельный обьект для сообщения об успехе
         }
 
 
-        @ExceptionHandler
+
+
+    @ExceptionHandler
         private ResponseEntity<PersonErrorResponse> handleException(PersonNotFoundException e){
             PersonErrorResponse response = new PersonErrorResponse("Person with this ID wasn't found",
                     System.currentTimeMillis());
@@ -67,6 +76,11 @@ public class PeopleController {
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
+    private Person convertToPerson(PersonDTO personDTO) {
+        return modelMapper.map(personDTO,Person.class);
+    }
 
-
+    private PersonDTO convertToPersonDTO(Person person){
+        return modelMapper.map(person,PersonDTO.class);
+    }
 }
